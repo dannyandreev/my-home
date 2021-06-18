55ac2190c476701863c42f1f24c4db2930e69944
dannyandreev <danny.andreev@gmail.com>
2021-06-18 12:35:36 -0700

    House Rules, Amenities, and Rent-A-Room done

--- main.js
+++ main.js
@@ -1,14 +1,41 @@
 var headerNavbar = document.getElementById("header-nav-bar")
 headerNavbar.addEventListener('click' , handleClick)
 
-for(page in pages){
-  if(pages[page].navlink){
-    headerNavbar.appendChild(createNavbarElements(pages[page]))
 
+createNavBar()
+constructHouseRules()
+constructAmenities()
 
-  }
+function createNavBar(){
+  for(page in pages){
+    if(pages[page].navlink){
+      headerNavbar.appendChild(createNavbarElements(pages[page]))
+    }
   pages[page].height = document.getElementById(pages[page].id).offsetHeight //needed for link color change based on scroll
+  }
 }
+
+
+
+function constructAmenities(){
+  var amenitieshtml = document.getElementById('amenities')
+  var amenities = house.amenities
+
+  for(amenitie in amenities) {
+    amenitieshtml.appendChild(createAttribute(amenities, amenitie))
+  }
+}
+
+function constructHouseRules(){
+  var ruleshtml = document.getElementById('rules')
+  var rules = house.rules
+
+  for(rule in rules) {
+    ruleshtml.appendChild(createAttribute(rules, rule))
+  }
+}
+
+
 
 function createNavbarElements(page){
 
@@ -31,4 +58,42 @@ function handleClick(event) {
     }
     event.target.className += " active"
   }
+}
+
+function createAttribute(attributes, attribute){
+  var newAttribute = document.createElement('div')
+  newAttribute.className = "flex text-font amenitie-container"
+
+  var imgContainer = document.createElement('div')
+  imgContainer.className="center-v"
+
+  var newAttributeIcon = document.createElement('img')
+  newAttributeIcon.setAttribute('src', attributes[attribute].imgURL)
+  newAttributeIcon.className="icon"
+
+  var newAttributeTextContainer = document.createElement('div')
+  newAttributeTextContainer.className="amenities-text-container center-v"
+
+  var newAttributeTextInnerContainer = document.createElement('div')
+  newAttributeTextInnerContainer.className = "main-font light"
+
+  var newAttributeTitle =document.createElement('div')
+  newAttributeTitle.className = "font-size-medium"
+  newAttributeTitle.textContent = attributes[attribute].title
+
+  var newAttributeText =document.createElement('div')
+  newAttributeText.className = "amenitie-description"
+  newAttributeText.textContent = attributes[attribute].description
+
+  newAttributeTextInnerContainer.appendChild(newAttributeTitle)
+  newAttributeTextInnerContainer.appendChild(newAttributeText)
+
+  newAttributeTextContainer.appendChild(newAttributeTextInnerContainer)
+
+  imgContainer.appendChild(newAttributeIcon)
+
+  newAttribute.appendChild(imgContainer)
+  newAttribute.appendChild(newAttributeTextContainer)
+
+  return newAttribute
 }
